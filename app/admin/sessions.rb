ea83ce74db25d611a5c5de969fba964d4409ca08
Dean Palm <dean451@gmail.com>
2018-12-13 10:43:52 -0600

    action item example, export to filemaker

--- app/admin/sessions.rb
+++ app/admin/sessions.rb
@@ -1,4 +1,9 @@
 ActiveAdmin.register Session do
   permit_params :title, :description, :text, :steps, :source, :difficulty, :tags, :hashtag, :format, :track
 
+
+   action_item :export, only: :show do
+    link_to 'Export To Filemaker', admin_sessions_path
+   end
+
 end
